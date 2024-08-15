73b4536c85d9d8bd84005fa22bfca314ba990cb4
mateuszNadolny <mateusznadolny00@gmail.com>
2024-08-15 17:21:16 +0200

    build: installed shadcn utilities

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
+import HeroSection from '@/components/landing-page/hero-section';
+
 export default function Home() {
   return (
-    <main className="flex h-screen min-h-screen flex-col items-center justify-center bg-color-primary">
-      <h1 className="text-color-light font-extrabold text-4xl">Form Speedrunner🏃‍➡️</h1>
-      <h3 className="text-color-light font-regular text-xl">
-        How <span className="text-color-teritary">fast</span> can you{' '}
-        <span className="text-color-teritary">submit a form</span>?
-      </h3>
+    <main className="flex h-screen min-h-screen flex-col items-center justify-center bg-color-primary overflow-hidden">
+      <HeroSection />
     </main>
   );
 }
---
+++ components/landing-page/hero-section.tsx
@@ -0,0 +1,23 @@
+import 'animate.css';
+import { Button } from '@/components/ui/button';
+
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <>
+      <h1 className="text-color-light font-extrabold text-4xl animate__animated animate__backInRight">
+        Form Speedrunner 🏃‍➡️
+      </h1>
+      <h3 className="animate__animated text-color-light font-regular text-xl animate__backInLeft">
+        How <span className="text-color-teritary font-bold">fast</span> can you
+        <span className="text-color-teritary font-bold"> submit a form</span>?
+      </h3>
+      <div className="animate__animated animate__backInUp animate__delay-1s mt-7">
+        <Button className="font-regular text-xl bg-color-teritary">{`Let's get started`}</Button>
+      </div>
+    </>
+  );
+};
+
+export default HeroSection;
