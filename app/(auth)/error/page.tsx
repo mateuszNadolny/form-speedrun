b66491a47dea4d476ff122ad418f7bad9cca5370
MateuszNadolny <mateusznadolny00@gmail.com>
2024-09-14 14:24:44 +0200

    feat: created error page

---
+++ app/(auth)/error/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+
+const ErrorPage = () => {
+  const router = useRouter();
+  return (
+    <main className="flex h-screen min-h-screen flex-col items-center justify-center bg-color-primary overflow-hidden">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-destructive mb-4">Oops! Something went wrong 🥲</h1>
+        <p className="text-lg text-muted-foreground mb-8">
+          {`We're sorry, but an error occurred while processing your request.`}
+        </p>
+        <Button
+          className="bg-color-teritary text-color-light hover:bg-color-secondary"
+          onClick={() => router.back()}>
+          Go Back
+        </Button>
+      </div>
+    </main>
+  );
+};
+
+export default ErrorPage;
--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,7 +1,7 @@
 import AuthTabs from '@/components/auth/auth-tabs';
 const SigninPage = () => {
   return (
-    <main className="flex h-screen min-h-screen flex-col items-center justify-center bg-color-primary overflow-hidden">
+    <main className="flex h-screen min-h-screen flex-col items-center justify-center bg-color-primary">
       <AuthTabs />
     </main>
   );
