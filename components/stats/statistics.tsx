287ff96db3d16051534527ea269fc99e12315b71
MateuszNadolny <mateusznadolny00@gmail.com>
2024-10-31 10:59:03 +0100

    style: Updated responsive styling

--- components/stats/statistics.tsx
+++ components/stats/statistics.tsx
@@ -6,7 +6,7 @@ import TotalTime from './total-time';
 const Statistics = () => {
   return (
     <ConfettiTrigger>
-      <div className="w-screen max-w-screen h-screen flex items-center justify-center p-32">
+      <div className="w-screen max-w-screen w-full flex flex-col lg:flex-row items-center justify-start pt-32 mb-20 lg:justify-center lg:p-32">
         <div className="w-full flex flex-col items-start justify-center rounded-xl">
           <TotalTime />
           <SplitTimes />
