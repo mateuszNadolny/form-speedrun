732b9eb17b2d62e54fb4fbfd9b123bcfe730f7fe
MateuszNadolny <mateusznadolny00@gmail.com>
2024-11-16 16:02:36 +0100

    feat: Added sign in button to unauthorized scoreboard

--- app/(main)/scoreboard/page.tsx
+++ app/(main)/scoreboard/page.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link';
+
+import { auth } from '@/auth';
+
+import PrimaryButton from '@/components/ui/primary-button';
 import { columns, Score } from '@/components/scoreboard/columns';
 import ScoreboardTable from '@/components/scoreboard/scoreboard-table';
 
 import { getGameScores } from '@/actions/get-game-scores';
-import PrimaryButton from '@/components/ui/primary-button';
-import Link from 'next/link';
-import { Play } from 'lucide-react';
+
+import { Play, User } from 'lucide-react';
 
 async function getData(): Promise<Score[]> {
   const scores = await getGameScores();
@@ -12,6 +16,7 @@ async function getData(): Promise<Score[]> {
 }
 
 const ScoreboardPage = async () => {
+  const session = await auth();
   const data = await getData();
   return (
     <section className="flex h-screen min-h-screen flex-col items-center justify-start touch-auto	overflow-scroll lg:overflow-hidden py-24">
@@ -21,12 +26,20 @@ const ScoreboardPage = async () => {
       </div>
       <ScoreboardTable columns={columns} data={data} />
       <div className="text-color-light text-sm my-6">{`Don't see your name? Keep practicing and climb the ranks!`}</div>
-      <PrimaryButton>
-        <Link href="/game" className="flex items-center gap-2">
-          <Play />
-          Play again
-        </Link>
-      </PrimaryButton>
+      <div className="flex gap-4">
+        <PrimaryButton>
+          <Link href="/game" className="flex items-center gap-2">
+            <Play />
+            Play again
+          </Link>
+        </PrimaryButton>
+        <PrimaryButton>
+          <Link href="/profile" className="flex items-center gap-2">
+            <User />
+            {session?.user ? 'Your ranking' : 'Sign in to save your score'}
+          </Link>
+        </PrimaryButton>
+      </div>
     </section>
   );
 };
