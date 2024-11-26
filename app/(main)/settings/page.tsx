061d8e5e993c3e48b5aa638a7ebf9ba4c32c8741
MateuszNadolny <mateusznadolny00@gmail.com>
2024-11-26 21:08:33 +0100

    style: Updated styling

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -1,10 +1,11 @@
-import ChangePasswordFormWrapper from '@/components/settings/change-password-form-wrapper';
 import DeleteAccountWrapper from '@/components/settings/delete-account-wrapper';
 
 const SettingsPage = () => {
   return (
-    <section className="flex flex-col w-full gap-8 h-screen max-h-screen py-20 px-5 lg:pt-26 lg:px-20 overflow-y-auto">
-      <ChangePasswordFormWrapper />
+    <section className="flex flex-col w-full items-center gap-8 h-screen max-h-screen py-20 px-5 lg:pt-26 lg:px-20 overflow-y-auto">
+      <h1 className="text-xl lg:text-4xl font-bold text-color-light text-center font-semibold font-heading">
+        Settings
+      </h1>
       <DeleteAccountWrapper />
     </section>
   );
