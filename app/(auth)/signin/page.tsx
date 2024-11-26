8c9374ddd7715c2de92532cc30106f942cb374c0
MateuszNadolny <mateusznadolny00@gmail.com>
2024-11-26 21:31:34 +0100

    fix: Fixed build issue Added suspsense to signin page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from 'react';
 import AuthWrapper from '@/components/auth/auth-wrapper';
 import BackButton from '@/components/ui/back-button';
+
 const SigninPage = () => {
   return (
     <section className="flex h-screen min-h-screen flex-col items-center justify-center">
       <BackButton text="Back to Form Speedrunner" href="/" />
-      <AuthWrapper />
+      <Suspense fallback={<div>Loading...</div>}>
+        <AuthWrapper />
+      </Suspense>
     </section>
   );
 };
